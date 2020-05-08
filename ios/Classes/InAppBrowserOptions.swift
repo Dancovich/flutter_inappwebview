0149144f22de1a57d836bf6db194b09f89b1d49e
Geraldo Sales <falecom@geraldosales.com.br>
2020-05-08 16:54:11 -0300

    Ajustando

--- ios/Classes/InAppBrowserOptions.swift
+++ ios/Classes/InAppBrowserOptions.swift
@@ -15,7 +15,6 @@ public class InAppBrowserOptions: Options {
     var toolbarTopBackgroundColor = ""
     var toolbarTopFixedTitle = ""
     var hideUrlBar = false
-    
     var toolbarBottom = true
     var toolbarBottomBackgroundColor = ""
     var toolbarBottomTranslucent = true
@@ -28,6 +27,62 @@ public class InAppBrowserOptions: Options {
     override init(){
         super.init()
     }
+
+    override func parse(options: [String: Any]) -> Options {
+        for optionKey in options.keys {
+            switch(optionKey) {
+                case "hidden":
+                    hidden = options[optionKey] as? Bool ?? hidden
+                case "toolbarTop":
+                    toolbarTop = options[optionKey] as? Bool ?? toolbarTop
+                case "toolbarTopBackgroundColor":
+                    toolbarTopBackgroundColor = options[optionKey] as? String ?? toolbarTopBackgroundColor
+                case "toolbarTopFixedTitle":
+                    toolbarTopFixedTitle = options[optionKey] as? String ?? toolbarTopFixedTitle
+                case "hideUrlBar":
+                    hideUrlBar = options[optionKey] as? Bool ?? hideUrlBar
+                case "toolbarBottom":
+                    toolbarBottom = options[optionKey] as? Bool ?? toolbarBottom
+                case "toolbarBottomBackgroundColor":
+                    toolbarBottomBackgroundColor = options[optionKey] as? String ?? toolbarBottomBackgroundColor
+                case "toolbarBottomTranslucent":
+                    toolbarBottomTranslucent = options[optionKey] as? Bool ?? toolbarBottomTranslucent
+                case "closeButtonCaption":
+                    closeButtonCaption = options[optionKey] as? String ?? closeButtonColor
+                case "closeButtonColor":
+                    closeButtonColor = options[optionKey] as? String ?? closeButtonColor
+                case "presentationStyle":
+                    presentationStyle = options[optionKey] as? Int ?? presentationStyle
+                case "transitionStyle":
+                    transitionStyle = options[optionKey] as? Int ?? transitionStyle
+                case "spinner":
+                    spinner = options[optionKey] as? Bool ?? spinner
+            default:
+                hidden = options["hidden"] as? Bool ?? hidden
+            }
+        }
+
+        return self
+    }
+
+    override func getHashMap() -> [String: Any] {
+        var options: [String: Any] = [:]
+        
+        options["hidden"] = hidden
+        options["toolbarTop"] = toolbarTop
+        options["toolbarTopBackgroundColor"] = toolbarTopBackgroundColor
+        options["toolbarTopFixedTitle"] = toolbarTopFixedTitle
+        options["hideUrlBar"] = hideUrlBar
+        options["toolbarBottom"] = toolbarBottom
+        options["toolbarBottomBackgroundColor"] = toolbarBottomBackgroundColor
+        options["toolbarBottomTranslucent"] = toolbarBottomTranslucent
+        options["closeButtonCaption"] = closeButtonCaption
+        options["closeButtonColor"] = closeButtonColor
+        options["presentationStyle"] = presentationStyle
+        options["transitionStyle"] = transitionStyle
+        options["spinner"] = spinner
+
+        return options
+    }
     
 }
-
--- ios/Classes/InAppWebViewOptions.swift
+++ ios/Classes/InAppWebViewOptions.swift
@@ -33,7 +33,6 @@ public class InAppWebViewOptions: Options {
     var transparentBackground = false
     var disableVerticalScroll = false
     var disableHorizontalScroll = false
-
     var disallowOverScroll = false
     var enableViewportScale = false
     var suppressesIncrementalRendering = false
@@ -53,7 +52,133 @@ public class InAppWebViewOptions: Options {
     override init(){
         super.init()
     }
-    
+
+    override func parse(options: [String: Any]) -> Options {
+        for optionKey in options.keys {
+            switch(optionKey) {
+                case "useShouldOverrideUrlLoading":
+                    useShouldOverrideUrlLoading = options[optionKey] as? Bool ?? useShouldOverrideUrlLoading
+                case "useOnLoadResource":
+                    useOnLoadResource = options[optionKey] as? Bool ?? useOnLoadResource
+                case "useOnDownloadStart":
+                    useOnDownloadStart = options[optionKey] as? Bool ?? useOnDownloadStart
+                case "clearCache":
+                    clearCache = options[optionKey] as? Bool ?? clearCache
+                case "userAgent":
+                    userAgent = options[optionKey] as? String ?? userAgent
+                case "applicationNameForUserAgent":
+                    applicationNameForUserAgent = options[optionKey] as? String ?? applicationNameForUserAgent
+                case "javaScriptEnabled":
+                    javaScriptEnabled = options[optionKey] as? Bool ?? javaScriptEnabled
+                case "debuggingEnabled":
+                    debuggingEnabled = options[optionKey] as? Bool ?? debuggingEnabled
+                case "javaScriptCanOpenWindowsAutomatically":
+                    javaScriptCanOpenWindowsAutomatically = options[optionKey] as? Bool ?? javaScriptCanOpenWindowsAutomatically
+                case "mediaPlaybackRequiresUserGesture":
+                    mediaPlaybackRequiresUserGesture = options[optionKey] as? Bool ?? mediaPlaybackRequiresUserGesture
+                case "verticalScrollBarEnabled":
+                    verticalScrollBarEnabled = options[optionKey] as? Bool ?? verticalScrollBarEnabled
+                case "horizontalScrollBarEnabled":
+                    horizontalScrollBarEnabled = options[optionKey] as? Bool ?? horizontalScrollBarEnabled
+                case "resourceCustomSchemes":
+                    resourceCustomSchemes = options[optionKey] as? [String] ?? resourceCustomSchemes
+                case "contentBlockers":
+                    contentBlockers = options[optionKey] as? [[String: [String : Any]]] ?? contentBlockers
+                case "minimumFontSize":
+                    minimumFontSize = options[optionKey] as? Int ?? minimumFontSize
+                case "useShouldInterceptAjaxRequest":
+                    useShouldInterceptAjaxRequest = options[optionKey] as? Bool ?? useShouldInterceptAjaxRequest
+                case "useShouldInterceptFetchRequest":
+                    useShouldInterceptFetchRequest = options[optionKey] as? Bool ?? useShouldInterceptFetchRequest
+                case "incognito":
+                    incognito = options[optionKey] as? Bool ?? incognito
+                case "cacheEnabled":
+                    cacheEnabled = options[optionKey] as? Bool ?? cacheEnabled
+                case "transparentBackground":
+                    transparentBackground = options[optionKey] as? Bool ?? transparentBackground
+                case "disableVerticalScroll":
+                    disableVerticalScroll = options[optionKey] as? Bool ?? disableVerticalScroll
+                case "disableHorizontalScroll":
+                    disableHorizontalScroll = options[optionKey] as? Bool ?? disableHorizontalScroll
+                case "disallowOverScroll":
+                    disallowOverScroll = options[optionKey] as? Bool ?? disallowOverScroll
+                case "enableViewportScale":
+                    enableViewportScale = options[optionKey] as? Bool ?? enableViewportScale
+                case "suppressesIncrementalRendering":
+                    suppressesIncrementalRendering = options[optionKey] as? Bool ?? suppressesIncrementalRendering
+                case "allowsAirPlayForMediaPlayback":
+                    allowsAirPlayForMediaPlayback = options[optionKey] as? Bool ?? allowsAirPlayForMediaPlayback
+                case "allowsBackForwardNavigationGestures":
+                    allowsBackForwardNavigationGestures = options[optionKey] as? Bool ?? allowsBackForwardNavigationGestures
+                case "allowsLinkPreview":
+                    allowsLinkPreview = options[optionKey] as? Bool ?? allowsLinkPreview
+                case "ignoresViewportScaleLimits":
+                    ignoresViewportScaleLimits = options[optionKey] as? Bool ?? ignoresViewportScaleLimits
+                case "allowsInlineMediaPlayback":
+                    allowsInlineMediaPlayback = options[optionKey] as? Bool ?? allowsInlineMediaPlayback
+                case "allowsPictureInPictureMediaPlayback":
+                    allowsPictureInPictureMediaPlayback = options[optionKey] as? Bool ?? allowsPictureInPictureMediaPlayback
+                case "isFraudulentWebsiteWarningEnabled":
+                    isFraudulentWebsiteWarningEnabled = options[optionKey] as? Bool ?? isFraudulentWebsiteWarningEnabled
+                case "selectionGranularity":
+                    selectionGranularity = options[optionKey] as? Int ?? selectionGranularity
+                case "dataDetectorTypes":
+                    dataDetectorTypes = options[optionKey] as? [String] ?? dataDetectorTypes
+                case "preferredContentMode":
+                    preferredContentMode = options[optionKey] as? Int ?? preferredContentMode
+                case "sharedCookiesEnabled":
+                    sharedCookiesEnabled = options[optionKey] as? Bool ?? sharedCookiesEnabled
+                case "automaticallyAdjustsScrollIndicatorInsets":
+                    automaticallyAdjustsScrollIndicatorInsets = options[optionKey] as? Bool ?? automaticallyAdjustsScrollIndicatorInsets
+            default:
+                useShouldOverrideUrlLoading = options["useShouldOverrideUrlLoading"] as? Bool ?? useShouldOverrideUrlLoading
+            }
+        }
+
+        return self
+    }
+
+    override func getHashMap() -> [String: Any] {
+        var options: [String: Any] = [:]
+
+        options["useShouldOverrideUrlLoading"] = useShouldOverrideUrlLoading
+        options["useOnLoadResource"] = useOnLoadResource
+        options["useOnDownloadStart"] = useOnDownloadStart
+        options["clearCache"] = clearCache
+        options["userAgent"] = userAgent
+        options["applicationNameForUserAgent"] = applicationNameForUserAgent
+        options["javaScriptEnabled"] = javaScriptEnabled
+        options["debuggingEnabled"] = debuggingEnabled
+        options["javaScriptCanOpenWindowsAutomatically"] = javaScriptCanOpenWindowsAutomatically
+        options["mediaPlaybackRequiresUserGesture"] = mediaPlaybackRequiresUserGesture
+        options["verticalScrollBarEnabled"] = verticalScrollBarEnabled
+        options["horizontalScrollBarEnabled"] = horizontalScrollBarEnabled
+        options["resourceCustomSchemes"] = resourceCustomSchemes
+        options["contentBlockers"] = contentBlockers
+        options["minimumFontSize"] = minimumFontSize
+        options["useShouldInterceptAjaxRequest"] = useShouldInterceptAjaxRequest
+        options["useShouldInterceptFetchRequest"] = useShouldInterceptFetchRequest
+        options["incognito"] = incognito
+        options["cacheEnabled"] = cacheEnabled
+        options["transparentBackground"] = transparentBackground
+        options["disableVerticalScroll"] = disableVerticalScroll
+        options["disableHorizontalScroll"] = disableHorizontalScroll
+        options["disallowOverScroll"] = disallowOverScroll
+        options["enableViewportScale"] = enableViewportScale
+        options["suppressesIncrementalRendering"] = suppressesIncrementalRendering
+        options["allowsAirPlayForMediaPlayback"] = allowsAirPlayForMediaPlayback
+        options["allowsBackForwardNavigationGestures"] = allowsBackForwardNavigationGestures
+        options["allowsLinkPreview"] = allowsLinkPreview
+        options["ignoresViewportScaleLimits"] = ignoresViewportScaleLimits
+        options["allowsInlineMediaPlayback"] = allowsInlineMediaPlayback
+        options["allowsPictureInPictureMediaPlayback"] = allowsPictureInPictureMediaPlayback
+        options["isFraudulentWebsiteWarningEnabled"] = isFraudulentWebsiteWarningEnabled
+        options["selectionGranularity"] = selectionGranularity
+        options["dataDetectorTypes"] = dataDetectorTypes
+        options["preferredContentMode"] = preferredContentMode
+        options["sharedCookiesEnabled"] = sharedCookiesEnabled
+        options["automaticallyAdjustsScrollIndicatorInsets"] = automaticallyAdjustsScrollIndicatorInsets
+
+        return options
+    }
 }
-
-
